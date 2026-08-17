03e5a913e462a83265d4fc15513a0a956303b617
Tahaseen Shaik <83743580+tshaik32@users.noreply.github.com>
2026-08-16 22:38:39 -0400

    standard updates to projects + resume

---
+++ static/project-data.js
@@ -0,0 +1,6 @@
+window.PORTFOLIO_PROJECTS = Object.freeze({
+  correctiveSharedAutonomy: Object.freeze({
+    repositoryUrl: "https://github.com/tahaseens/corrective-shared-autonomy",
+    repositoryStatus: "coming-soon"
+  })
+});
